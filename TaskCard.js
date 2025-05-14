9e9471d274199f2fbec14b64c038d838fe6ab90e
Veenasri22 <136073384+Veenasri22@users.noreply.github.com>
2025-05-14 14:16:20 +0530

    upload

--- TaskCard.js
+++ TaskCard.js
@@ -1,8 +1,19 @@
-// components/TaskCard.js
-import React from "react";
+import React from 'react';
 
-function TaskCard({ task }) {
-  return <div className="task-card">{task.content}</div>;
+export default function TaskCard({ task, onDelete, onMove, allColumns, currentColumn }) {
+  return (
+    <div style={{ border: '1px solid black', margin: '0.5rem 0', padding: '0.5rem' }}>
+      <p>{task}</p>
+      <button onClick={onDelete}>Delete</button>
+      <div>
+        {allColumns
+          .filter(col => col !== currentColumn)
+          .map(col => (
+            <button key={col} onClick={() => onMove(col)}>
+              Move to {col}
+            </button>
+          ))}
+      </div>
+    </div>
+  );
 }
-
-export default TaskCard;
